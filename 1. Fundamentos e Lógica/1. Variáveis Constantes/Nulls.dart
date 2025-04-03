f580d0fd567f5a6387af3ae7fa1392ab397424ef
rayMSilva <raydasilvaknapp@hotmail.com>
2025-04-03 18:02:58 -0300

    alterações em vars

---
+++ 1. Fundamentos e Lógica/1. Variáveis Constantes/Nulls.dart
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+
+
+String? nome; //Tipo nulo. aceita 2 tipos de valores nulo e String
+String name = 'Bob'; //Tipo não nulo. aceita 1 tipo de valor e precisa ser instanciado ou receber algum valor
---
+++ 1. Fundamentos e Lógica/1. Variáveis Constantes/Vars.dart
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+
+var name = 'Bob'; //Declarando e Inicializando a Variável name com referencia String com o valor Bob, sem inflicitar o tipo
+dynamic bob = 'Bob';// Declarando e Inicializando a Variável bob sem especificar o tipo da variável, isso serve para variáveis que podem receber mais de 1 tipo 
+String nome = 'Bob';// Declarando e Inicializando a Variável nome com tipo da variável inflicto
+
