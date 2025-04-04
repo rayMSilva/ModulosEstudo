5286369aa3c0c4fec8c6f1cbf31de89d1baf6958
rayMSilva <raydasilvaknapp@hotmail.com>
2025-04-04 06:48:18 -0300

    começando a estudar modificadores de variável

--- 1. Fundamentos e Lógica/1. Variáveis Constantes/Vars.dart
+++ 1. Fundamentos e Lógica/1. Variáveis Constantes/Vars.dart
@@ -8,8 +8,14 @@
 
 
 
-
+// Variáveis em Dart(de tipo estático ou dinâmico)
 var name = 'Bob'; //Declarando e Inicializando a Variável name com referencia String com o valor Bob, sem inflicitar o tipo
 dynamic bob = 'Bob';// Declarando e Inicializando a Variável bob sem especificar o tipo da variável, isso serve para variáveis que podem receber mais de 1 tipo 
 String nome = 'Bob';// Declarando e Inicializando a Variável nome com tipo da variável inflicto
 
+//Nulos e não nulos
+String? nome1; //Tipo nulo. aceita 2 tipos de valores nulo e String
+String name2 = 'Bob'; //Tipo não nulo. aceita 1 tipo de valor e precisa ser instanciado ou receber algum valor
+
+//Late Variáveis
+late int num1;
