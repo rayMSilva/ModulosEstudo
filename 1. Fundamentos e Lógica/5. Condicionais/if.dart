4e842aef9e707e371d82a8e419e37d4962033fab
rayMSilva <raydasilvaknapp@hotmail.com>
2025-04-07 23:06:05 -0300

    alterações nas pastas de condições e laços

--- 1. Fundamentos e Lógica/5. Condicionais/if.dart
+++ 1. Fundamentos e Lógica/5. Condicionais/if.dart
@@ -1,16 +1,44 @@
 
 
+import 'dart:io';
+
 class Pessoas{
   
-  late String _nome;
-  late String _idade;
+  late String? _nome;
+  late String? _idade;
 
   Pessoas(this._nome, this._idade);
 
   //getters & setters
 
   String caracteristicas(Pessoas){
-    return 'Nome: $Pessoas._nome  || idade: $Pessoas._idade';
+    return '\n\nNome: ${Pessoas._nome}  || idade: ${Pessoas._idade}';
+  }
+
+}
+
+
+void main()
+{
+  try{
+    stdout.write('Digite seu Nome: ');
+    String? nome = stdin.readLineSync();
+    stdout.write('Digite sua Idade: ');
+    String? idade = stdin.readLineSync();
+
+
+    Pessoas pessoa1 = Pessoas(nome, idade);
+
+
+    print(pessoa1.caracteristicas(pessoa1));
+  }
+  catch(e)
+  {
+    print('Erro:\n\n ${e}');
+  }
+  finally
+  {
+    print('\n\nObrigado pela atenção');
   }
 
 }
